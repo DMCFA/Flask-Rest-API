69d1be55594e858f46e9cf616cebc94c9be66a12
Duarte Almeida <duartemalmeida@gmail.com>
2021-03-31 18:13:01 +0100

    add all the routes for CRUDE functionality and create db file

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ db = SQLAlchemy(app)
 # marshmallow
 ma = Marshmallow(app)
 
+
 # Class
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -29,5 +30,77 @@ class Product(db.Model):
         self.quantity = quantity
 
 
+# Product Schema
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'description', 'price', 'quantity')
+
+
+# Initialize Schema
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
+
+
+# Create a Product
+@app.route('/product', methods=['POST'])
+def add_product():
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    quantity = request.json['quantity']
+
+    new_product = Product(name, description, price, quantity)
+
+    db.session.add(new_product)
+    db.session.commit()
+
+    return product_schema.jsonify(new_product)
+
+
+# Get All Products
+@app.route('/product', methods=['GET'])
+def get_products():
+    all_products = Product.query.all()
+    result = products_schema.dump(all_products)
+    return jsonify(result)
+
+#Get Single Product
+@app.route('/product/<id>', methods=['GET'])
+def get_product(id):
+    product = Product.query.get(id)
+    return product_schema.jsonify(product)
+
+
+# Update a Product
+@app.route('/product/<id>', methods=['PUT'])
+def update_product(id):
+    product = Product.query.get(id)
+
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    quantity = request.json['quantity']
+
+    product.name = name
+    product.description = description
+    product.price = price
+    product.quantity = quantity
+
+    new_product = Product(name, description, price, quantity)
+
+    db.session.commit()
+
+    return product_schema.jsonify(product)
+
+#Delete Product
+@app.route('/product/<id>', methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+
+    return product_schema.jsonify(product)
+
+# Run Server
 if __name__ == '__main__':
     app.run(debug=True)
